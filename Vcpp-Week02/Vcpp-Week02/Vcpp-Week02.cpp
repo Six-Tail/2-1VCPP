cab8d0ac80e38153d8bb2a7fb69b6a4bc8c1cc36
Six_Tail <qowlgh1215@naver.com>
2023-09-13 18:31:19 +0900

    [2주차 과제]

--- Vcpp-Week02/Vcpp-Week02/Vcpp-Week02.cpp
+++ Vcpp-Week02/Vcpp-Week02/Vcpp-Week02.cpp
@@ -1,45 +1,50 @@
 ﻿#include <iostream>
-#include <conio.h>
+#include <conio.h> // getch 함수를 사용하기 위해 필요
 
 using namespace std;
 
-int main()
-{
-    cout << "배지호의 행복한 VC++과제" << endl; //스플래시 이미지 출력
+int main() {
+    char input;
 
-    char command = 0; //초기 커맨드값은 0
+    cout << "배지호의 행복한 VC++과제" << endl;
 
     while (true) {
-        cout << "command>" << command; //커맨드 입력 및 커서 출력
+        cout << "command>";
 
-        char input = _getch(); //키 입력 받기
+        string command = "";
+        
+        while (true) {
+            input = _getch(); // 키 입력을 받음
 
-        switch (input) { //입력된 키값에 따라 프로그램 실행
-
-        case 'q': //q: qwer 화이팅
-            cout << "qwer 화이팅" << endl;
-            break;
-
-        case 'w': //w: 과제 너무 좋다
-            cout << "과제 너무 좋다" << endl;
-            break;
-
-        case 'e': //e: 담주부턴 과제량 3배다
-            cout << "담주부턴 과제량 3배다" << endl;
-            break;
-
-        case 'r': //r: 행복합니다.
-            cout << "행복합니다." << endl;
-            break;
-
-        case 27: //ESC키를 누르면 즉시 프로그램이 종료하도록 만든다. (ESC키의 아스키코드는 27)
-            return 0; //프로그램 종료
-
-        default: //다른 글자를 입력하면 오류메시지와 함께 다시 입력하도록 안내한다.
-            cout << "\n오류: 잘못된 문자입니다. q,w,e,r중에 하나를 입력하세요." << endl;
+            if (input == 27) { // ESC 키를 누르면 프로그램 종료
+                cout << "\n프로그램을 종료합니다." << endl;
+                return 0;
+            }
+            else if (input == '\r') { // 엔터 키를 누르면 명령 실행
+                break;
+            }
+            else {
+                command += input;
+                cout << input;
+            }
+        }
+        
+        if (command == "q") { //q를 입력
+            cout << "\nqwer 화이팅" << endl;
+        }
+        else if (command == "w") { //w를 입력
+            cout << "\n과제 너무 좋다" << endl;
+        }
+        else if (command == "e") { //e를 입력
+            cout << "\n담주부턴 과제량 3배다" << endl;
+        }
+        else if (command == "r") { //r을 입력
+            cout << "\n행복합니다." << endl;
+        }
+        else { //그외를 입력
+            cout << "\nFatal Error: q,w,e,r중에 하나를 입력하세요." << endl;
         }
     }
+
     return 0;
 }
-
-
